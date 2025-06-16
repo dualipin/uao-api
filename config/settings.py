27341ecb77e4b85f42d22db1cd290c3d35fac953
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2025-06-16 13:12:31 -0600

    feat: Configure database settings for PostgreSQL and add psycopg2 dependency

--- config/settings.py
+++ config/settings.py
@@ -116,12 +116,24 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if(env('DB_ENGINE', default='sqlite') == 'postgres'):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": env("DB_NAME", default="uao_db"),
+            "USER": env("DB_USER", default="uao_user"),
+            "PASSWORD": env("DB_PASSWORD", default="uao_password"),
+            "HOST": env("DB_HOST", default="localhost"),
+            "PORT": env("DB_PORT", default="5432"),
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
